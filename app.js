baf2b5ea6e3bb67f79e0a20de13470b0f8014f08
Peymanrad <peymanradmo@gmail.com>
2022-05-27 22:49:50 +0430

    Added show and hide functionality to favorites modal

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ const showModalBtn = $.querySelector('.show_favorites')
 const favoritesModal = $.querySelector('.favoriteModal')
 const closeModalBtn = $.querySelector('.close_btn')
 const modalOverlay = $.querySelector('.overlay')
+
+function showModal() {
+    favoritesModal.style.top = '30%'
+    modalOverlay.style.display = 'block'
+}
+function hideModal() {
+    favoritesModal.style.top = '-100%'
+    modalOverlay.style.display = 'none'
+}
+showModalBtn.addEventListener('click',showModal)
+closeModalBtn.addEventListener('click',hideModal)
+modalOverlay.addEventListener('click',hideModal)
+$.addEventListener('keydown',function(e) {
+    if (e.key === 'Escape') {
+        hideModal()
+    }
+})
